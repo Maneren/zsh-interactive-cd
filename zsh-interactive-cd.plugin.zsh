bcb9b74d1acbb87f315ad66a3625c462db65c2d0
Maneren <49210777+Maneren@users.noreply.github.com>
2021-12-17 00:14:08 +0100

    fixes, simplifications, removed tmux stuff

--- zsh-interactive-cd.plugin.zsh
+++ zsh-interactive-cd.plugin.zsh
@@ -6,41 +6,33 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-__zic_fzf_prog() {
-    if [[ -n "$TMUX_PANE" && "${FZF_TMUX:-0}" != 0 && ${LINES:-40} -gt 15 ]]; then
-        echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}"
-    else
-        echo "fzf"
-    fi
-}
-
 __zic_calc_lenght() {
     if [ "$1" = "/" ]; then
         echo 0
     else
-        echo $(echo -n "$1" | wc -c)
+        echo -n "$1" | wc -c
     fi
 }
 
 __zic_list_subdirs() {
     local length=$(__zic_calc_lenght "$1")
     
-    local regex="$2"
-    local grep_opts="-E"
+    local grep_opts
     if [ "$zic_case_insensitive" = "true" ]; then
-        grep_opts="-iE"
+        grep_opts="-i"
     fi
     
     # lists subdirs
     # removes base path
     # filters by the regex
-    local subdirs=$(
-        find -L "$1" -maxdepth 1 -type d 2>/dev/null \
-        | command cut -b $(( ${length} + 2 ))- \
-        | command grep "$grep_opts" "$regex" \
-    )
-    
-    echo "$subdirs"
+    # TODO: experiment with removing the grep and using only `find -regex`
+    find -L "$1" -mindepth 1 -maxdepth 1 -type d 2>/dev/null \
+    | command cut -b $(( ${length} + 2 ))- \
+    | command grep "$grep_opts" -E "$2" \
+}
+
+__zic_regex_escape() {
+    sed 's/[^^]/[&]/g; s/\^/\\^/g' <<< "$1"
 }
 
 __zic_matched_subdir_list() {
@@ -50,7 +42,7 @@ __zic_matched_subdir_list() {
         
         # if $dir isn't just /, remove the traling /
         if [[ "$dir" != / ]]; then
-            dir="${dir: : -1}"
+            dir="${dir:0:-1}"
         fi
         
         local regex;
@@ -60,45 +52,48 @@ __zic_matched_subdir_list() {
             regex="^[^.].*$"
         fi
         
-        echo $(__zic_list_subdirs "$dir" "$regex")
+        __zic_list_subdirs "$dir" "$regex"
         
         return
     fi
     
-    local seg=$(basename -- "$1" ) # | sed 's/[^^]/[&]/g; s/\^/\\^/g'
+    # escape characters in the basename to be regex-safe
+    # (can be bypassed, but those chars that can't be in filnames anyway)
+    local seg=$(basename -- "$1")
     local dir=$(dirname -- "$1")
-    
-    local starts_with_seg=$(
-        local regex;
-        if [ "$zic_ignore_dot" = "true" ]; then
-            regex="^[.]?$seg.*$"
-        else
-            regex="^$seg.*$"
-        fi
-        
-        echo $(__zic_list_subdirs "$dir" "$regex")
-    )
+    local escaped=$(__zic_regex_escape $seg)
+    local regex
+
+    if [ "$zic_ignore_dot" = "true" ]; then
+        regex="^[.]?$escaped.*$"
+    else
+        regex="^$escaped.*$"
+    fi
+
+    local starts_with_seg=$(__zic_list_subdirs "$dir" "$regex")
     
     if [ -n "$starts_with_seg" ]; then
         echo "$starts_with_seg"
         return
     fi
-    
-    local regex;
-    if [[ "$zic_ignore_dot" == "true" || $seg[1] == "." ]]; then
-        regex="^.*$seg.*$"
+
+    # if first character of input ($1) is .,
+    # force starting . in the regex
+    if [ "${seg:0:1}" = "." ]; then
+        escaped=$(__zic_regex_escape "${seg:1}")
+        regex="^[.].*$escaped.*$"
+    elif [ "$zic_ignore_dot" = "true"  ]; then
+        regex="^.*$escaped.*$"
     else
-        regex="^[^.].*$seg.*$"
+        regex="^[^.].*$escaped.*$"
     fi
     
-    echo $(__zic_list_subdirs "$dir" "$regex")
+    __zic_list_subdirs "$dir" "$regex"
 }
 
 __zic_fzf_bindings() {
     autoload is-at-least
-    local fzf=$(__zic_fzf_prog)
-    
-    if $(is-at-least '0.21.0' $(${=fzf} --version)); then
+    if $(is-at-least '0.21.0' $(fzf --version)); then
         echo 'shift-tab:up,tab:down,bspace:backward-delete-char/eof'
     else
         echo 'shift-tab:up,tab:down'
@@ -106,11 +101,10 @@ __zic_fzf_bindings() {
 }
 
 _zic_list_generator() {
-    echo $(__zic_matched_subdir_list "${(Q)@[-1]}")
+    __zic_matched_subdir_list "${(Q)@[-1]}"
 }
 
 _zic_complete() {
-set -x
     setopt localoptions nonomatch
     
     local list=$(_zic_list_generator $@ | xargs -n 1 | sort -fiu)
@@ -120,28 +114,24 @@ set -x
         return
     fi
     
-    local fzf=$(__zic_fzf_prog)
-    
     local match
     # if there is only one match return it
     # else run fzf
-    if [ $(echo $list | wc -l) = 1 ]; then
-        match=${(q)list}
+    if [ $(wc -l <<< "$list") = 1 ]; then
+        match="${(q)list}"
     else
-        local fzf_opts="--height ${FZF_TMUX_HEIGHT:-40%} \
+        local fzf_opts="--height ${FZF_TMUX_HEIGHT:-"40%"} \
             --reverse $FZF_DEFAULT_OPTS $FZF_COMPLETION_OPTS \
-            --bind '$__zic_fzf_bindings_cache'"
+            --bind '$(__zic_fzf_bindings)'"
         
-        # call fzf with $list of options and save the quoted result on single line
-        match=$(
-            echo -n $(echo $list | FZF_DEFAULT_OPTS=$fzf_opts ${=fzf})
-        )
+        # call fzf with $list of options
+        match=$(echo -n $(FZF_DEFAULT_OPTS=$fzf_opts fzf <<< "$list"))
     fi
     
     match=${match% } # remove trailing space
     if [ -n "$match" ]; then
         local tokens=(${(z)LBUFFER}) # split LBUFFER into words
-        local command="${tokens[1]}"
+        local cmd="${tokens[1]}"
         local input="${tokens[2]}"
         
         local base="${@}"
@@ -171,7 +161,7 @@ set -x
         fi
         
         # append match to LBUFFER (base ends with a `/`)
-        LBUFFER="${command} ${base}${match}/"
+        LBUFFER="${cmd} ${base}${match}/"
     fi
     
     zle redisplay
@@ -179,13 +169,12 @@ set -x
 }
 
 zic-completion() {
-    set -x
-
     setopt localoptions noshwordsplit noksh_arrays noposixbuiltins
     
     local tokens=(${(z)LBUFFER})
-    local cmd=${tokens[1]}
-    local input=${tokens[2,${#tokens}]/#\~/"$HOME"}
+    local cmd="${tokens[1]}"
+    local input="${tokens[2,${#tokens}]}"
+    input=${input/#\~/$HOME}
     
     if [ "$cmd" != "cd" ]; then
         zle ${__zic_default_completion:-expand-or-complete}
@@ -204,8 +193,6 @@ zic-completion() {
     [[ $binding =~ 'undefined-key' ]] || __zic_default_completion=$binding[(s: :w)2]
     unset binding
 }
-
-export __zic_fzf_bindings_cache=$(__zic_fzf_bindings)
 
 zle -N zic-completion
 if [ -z $zic_custom_binding ]; then
