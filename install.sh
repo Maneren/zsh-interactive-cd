4d373f1a22560ca6fa9d39fdba3f933796098f64
Maneren <49210777+Maneren@users.noreply.github.com>
2022-02-20 20:19:42 +0100

    create basic install script

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
-# TODO
+#!/bin/sh
+
+mkdir -p ./bin
+
+(
+  cd ./zic-list-dirs || exit 1
+  cargo b --release
+)
+
+mv ./zic-list-dirs/target/release/zic-list-dirs ./bin/
+rm -rf ./zic-list-dirs/target
