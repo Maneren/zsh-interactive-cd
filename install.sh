d7d16f73e4ec830dc5c0362f7cf0315f68bce9ac
Maneren <49210777+Maneren@users.noreply.github.com>
2022-02-21 20:46:00 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 mkdir -p ./bin
 
 (
-  cd ./zic-list-dirs || exit 1
-  cargo b --release
+cd ./zic-list-dirs || exit 1
+cargo b --release
 )
 
-mv ./zic-list-dirs/target/release/zic-list-dirs ./bin/
-# rm -rf ./zic-list-dirs/target
+cp ./zic-list-dirs/target/release/zic-list-dirs ./bin/
+rm -rf ./zic-list-dirs/target
