a4449cf5051c4c4923cbd220d860128689aae5c1
Maneren <49210777+Maneren@users.noreply.github.com>
2022-12-10 01:01:45 +0100

    format the plugin file

--- zsh-interactive-cd.plugin.zsh
+++ zsh-interactive-cd.plugin.zsh
@@ -9,37 +9,37 @@
 PATH="$PATH:${0:a:h}/bin"
 
 if ! which zic-list-dirs 2>&1 >/dev/null; then
-  echo "zsh-interactive-cd: Binary not found" >&2
-  return 1
+    echo "zsh-interactive-cd: Binary not found" >&2
+    return 1
 fi
 
 [ -z $zic_custom_binding ] && zic_custom_binding='^I' # default is TAB
 
 if [ -z $__zic_default_completion ]; then
-  __binding=$(bindkey $zic_custom_binding)
-  
-  # if the key isn't bound to anything use ZSH's default completion
-  # else use the set completion
-  __zic_default_completion=$(
-    [ $__binding = 'undefined-key' ] && echo "expand-or-complete" || echo $__binding[(s: :w)2]
-  )
-  
-  unset __binding
+    __binding=$(bindkey $zic_custom_binding)
+
+    # if the key isn't bound to anything use ZSH's default completion
+    # else use the set completion
+    __zic_default_completion=$(
+        [ $__binding = 'undefined-key' ] && echo "expand-or-complete" || echo $__binding[(s: :w)2]
+    )
+
+    unset __binding
 fi
 
 function zic-completion() {
-  local output
-  output=$(zic-list-dirs "$LBUFFER" "${(e)LBUFFER}") # second argument is with expanded variables
-  
-  if [ ! $? = 0 ]; then
-    zle $__zic_default_completion
-    return
-  fi
-  
-  LBUFFER="${output}"
-  
-  zle redisplay
-  typeset -f zle-line-init >/dev/null && zle zle-line-init
+    local output
+    output=$(zic-list-dirs "$LBUFFER" "${(e)LBUFFER}") # second argument is with expanded variables
+
+    if [ ! $? = 0 ]; then
+        zle $__zic_default_completion
+        return
+    fi
+
+    LBUFFER="${output}"
+
+    zle redisplay
+    typeset -f zle-line-init >/dev/null && zle zle-line-init
 }
 
 zle -N zic-completion
